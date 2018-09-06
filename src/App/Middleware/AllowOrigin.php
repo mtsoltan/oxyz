55a16e02e2da459b8fa3083bb4ca45553645bbe1
mtsoltan <mahmoud.t.soltan@gmail.com>
2018-09-06 09:26:51 +0200

    Fixed cross origin request handling.

--- src/App/Middleware/AllowOrigin.php
+++ src/App/Middleware/AllowOrigin.php
@@ -15,9 +15,21 @@ class AllowOrigin
         $this->di = $di;
     }
 
+    /**
+     * @param \Slim\Http\Request $request
+     * @param \Slim\Http\Response $response
+     * @param callable $next Next middleware to be called.
+     * @return callable
+     */
     public function __invoke($request, $response, $next)
     {
-        // $response = $response->withHeader('Access-Control-Allow-Origin', '*'); // Allow www, ., and other subdomains to use serve.
+        $origin = implode($request->getHeader('Origin'));
+        $response = $response->withHeader('Vary', 'Origin');
+        if (preg_match('%'.$this->di['config']['site.origin'].'%', $origin)) {
+            $response = $response->withHeader('Access-Control-Allow-Origin', $origin);
+        } else {
+            $response = $response->withHeader('Access-Control-Allow-Origin', $this->di['config']['site.site_url']);
+        }
 
         return $next($request, $response); // Call the next middleware
     }
